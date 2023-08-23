6e38a12c4de9da6fdec3fd65bda8e9fc285961f4
Gaurav Mahajan <142982401+gaurv4@users.noreply.github.com>
2023-08-23 15:34:24 +0530

    files added first
    currently in working mode trying to make it responsive and add features in it

---
+++ main.js
@@ -0,0 +1,8 @@
+
+var typed = new Typed(".text", {
+    strings: ["Frontend Developer.", "Web Developer.", "UI Designer.", "Java Developer."],
+    typeSpeed: 70,
+    backSpeed: 40,
+    loop: true
+
+})
