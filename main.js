6e9b04dad90ef8e69939e12fd8711ea621caabb1
Gaurav Mahajan <142982401+gaurv4@users.noreply.github.com>
2023-08-26 18:39:18 +0530

    responsive menu added

--- main.js
+++ main.js
@@ -6,3 +6,13 @@ var typed = new Typed(".text", {
     loop: true
 
 })
+
+const mobile_nav = document.querySelector(".mobile-navbar-btn");
+const nav_header = document.querySelector(".header");
+
+const toggleNavbar = () => {
+  // alert("Plz Subscribe ");
+  nav_header.classList.toggle("active");
+};
+
+mobile_nav.addEventListener("click", () => toggleNavbar());
